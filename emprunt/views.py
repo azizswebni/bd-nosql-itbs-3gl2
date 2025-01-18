84771617b31b2465900536ecec6787a1f755cffc
Aziz SOUABNI <57048839+azizswebni@users.noreply.github.com>
2025-01-18 10:40:03 +0000

    add scheduler

--- emprunt/views.py
+++ emprunt/views.py
@@ -4,13 +4,23 @@ from bson import ObjectId
 from . import app
 from flask.views import MethodView
 
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+def make_document_available(document_id):
+    from abonnee.models import Abonnes, Documents, Emprunts
+    document = Documents.objects(id=document_id).first()
+    if document:
+        document.update(set__disponible=True)
+        print(f"Document {document_id} is now available.")
+
 @app.route('/insert_emprunt', methods=['POST'])
 def insert_emprunt():
     data = request.json
 
-    # if not all(k in data for k in ["abonne_id", "document_id", "date_retour_prevue"]):
-    #     return jsonify({"message": "Les champs 'abonne_id', 'document_id' et 'date_retour_prevue' sont obligatoires"}), 400
-    from abonnee.models import Abonnes, Documents, Emprunts
     abonne = Abonnes.objects(id=data["abonne_id"]).first()
     document = Documents.objects(id=data["document_id"]).first()
 
@@ -23,11 +33,21 @@ def insert_emprunt():
         date_retour_prevue=data["date_retour_prevue"]
     )
 
-    #document.update(set__disponible=False)
-
     nouvel_emprunt.save()
+    document.update(set__disponible=False)
+
+    date_retour_prevue = datetime.strptime(data["date_retour_prevue"], "%Y-%m-%d %H:%M:%S")
+    scheduler.add_job(
+        make_document_available,
+        trigger="date",
+        run_date=date_retour_prevue,
+        args=[str(document.id)],
+        id=f"make_document_available_{document.id}",
+        replace_existing=True
+    )
 
     return jsonify({"message": "Emprunt ajouté avec succès", "id": str(nouvel_emprunt.id)}), 201
+
 
 @app.route('/get_emprunts', methods=['GET'])
 def get_emprunts():
@@ -36,18 +56,19 @@ def get_emprunts():
     abonnes = Abonnes.objects()
     documents = Documents.objects()
 
-    emprunts_list = []
-    for emprunt in emprunts:
-        emprunts_list.append({
-            "_id": str(emprunt.id),
-            "abonne_id": str(emprunt.abonne.id),
-            "document_id": str(emprunt.document.id),
-            "date_emprunt": emprunt.date_emprunt.isoformat(),
-            "date_retour_prevue": emprunt.date_retour_prevue.isoformat(),
-            "statut": emprunt.statut
-        })
+    documents_list = []
+    for doc in documents:
+        if doc.disponible:
+            documents_list.append({
+            "_id": str(doc.id),
+            "titre": doc.titre,
+            "type": doc.type,
+            "auteur": doc.auteur,
+            "date_publication": doc.date_publication.isoformat(),
+            "disponible": doc.disponible
+            })
 
-    return render_template('emp.html', emprunts=emprunts,abonnes=abonnes,documents=documents)
+    return render_template('emp.html', emprunts=emprunts,abonnes=abonnes,documents=documents_list)
 
 @app.route('/get_emprunt/<id_emprunt>', methods=['GET'])
 def get_emprunt(id_emprunt):
